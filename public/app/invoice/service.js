70014662f5832b9b60760ba6b45b049a67e65dee
laislopes <laisjkl@hotmail.com>
2023-07-07 14:55:18 -0300

    Implements functions composition.

--- public/app/invoice/service.js
+++ public/app/invoice/service.js
@@ -1,20 +1,34 @@
 import { handleStatus } from '../utils/promise-helpers.js';
-import './utils/array-helpers.js';
+import { partialize, compose} from '../utils/operators.js';
+import '../utils/array-helpers.js';
 
 const API = 'http://localhost:3000/invoices';
 
-const sumItems = code => invoices => invoices
-        .$flatMap(invoice => invoice.items)
-        .filter(item => item.code === code)
-        .reduce((total, item) => total + item.value, 0);
-
+const getItemsFromInvoices = invoices => invoices.$flatMap(invoice => invoice.items);
+const filterItemsByCode = (code, items) => items.filter(item => item.code === code);
+const sumItemsValue = items =>  items.reduce((total, item) => total + item.value, 0);
+    
 export const invoicesService = {
 
     listAll(){
-        return fetch(API).then(handleStatus);
+        return fetch(API)
+            .then(handleStatus)
+            .catch(error => {
+                console.log(error);
+                return Promise.reject('Was not possible to get the invoices');
+            });
     },
 
     sumItems(code){
-        this.listAll().then(sumItems(code));
+        const filterItems = partialize(filterItemsByCode, code);
+
+        return this.listAll()
+                    .then(invoices => 
+                        sumItemsValue(
+                            filterItems(
+                                getItemsFromInvoices(invoices)
+                                )
+                        )
+                    );
     }
 };
