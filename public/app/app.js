09bcd23e96b73aa8067fa4b07c3385c837437f63
laislopes <laisjkl@hotmail.com>
2023-07-07 15:24:38 -0300

    Implements TakeUntil.

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,15 @@
 import { log } from './utils/promise-helpers.js';
 import { invoicesService as service } from './invoice/service.js';
+import { takeUntil } from './utils/operators.js';
+
+const operation = takeUntil(3, () =>
+    service
+        .sumItems('2143')
+        .then(console.log)
+        .catch(console.log)
+);
+
 
 document
 .querySelector('#myButton')
-.onclick = () => 
-    service
-    .sumItems('2143')
-    .then(console.log)
-    .catch(console.log);
+.onclick = operation;
