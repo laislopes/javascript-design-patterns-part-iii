3745773b086b32d270cf89ea8a0fce34f3315e56
laislopes <laisjkl@hotmail.com>
2023-07-07 15:39:39 -0300

    Composition of takeUntil and debounceTime.

--- public/app/app.js
+++ public/app/app.js
@@ -2,13 +2,17 @@ import { log } from './utils/promise-helpers.js';
 import { invoicesService as service } from './invoice/service.js';
 import { takeUntil, debounceTime } from './utils/operators.js';
 
-const action = debounceTime(500, takeUntil(3, () => 
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+);
+
+const action = operations(() => 
     service
     .sumItems('2143')
     .then(console.log)
     .catch(console.log)
-));
-
+);
 
 document
 .querySelector('#myButton')
