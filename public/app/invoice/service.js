96a94e6a0e9cf99abdaea17d3d9a3c579f9366ac
laislopes <laisjkl@hotmail.com>
2023-07-10 16:45:02 -0300

    Implements Monads.

--- public/app/invoice/service.js
+++ public/app/invoice/service.js
@@ -1,18 +1,20 @@
 import { handleStatus } from '../utils/promise-helpers.js';
 import { partialize, compose, pipe} from '../utils/operators.js';
 import '../utils/array-helpers.js';
-
+import { Maybe } from '../utils/maybe.js';
 const API = 'http://localhost:3000/invoices';
 
-const getItemsFromInvoices = invoices => invoices.$flatMap(invoice => invoice.items);
-const filterItemsByCode = (code, items) => items.filter(item => item.code === code);
-const sumItemsValue = items =>  items.reduce((total, item) => total + item.value, 0);
+const getItemsFromInvoices = invoicesM => invoicesM.map(invoices => invoices.$flatMap(invoice => invoice.items));
+const filterItemsByCode = (code, itemsM) => itemsM.map(items => items.filter(item => item.code === code));
+const sumItemsValue = itemsM =>  itemsM.map(items => items.reduce((total, item) => total + item.value, 0));
     
 export const invoicesService = {
 
     listAll(){
         return fetch(API)
             .then(handleStatus)
+            // .then(invoices => Maybe.of(null))
+            .then(invoices => Maybe.of(invoices))
             .catch(error => {
                 console.log(error);
                 return Promise.reject('Was not possible to get the invoices');
@@ -24,6 +26,7 @@ export const invoicesService = {
         const sumItems = pipe(getItemsFromInvoices, filterItems, sumItemsValue);
 
         return this.listAll()
-                    .then(sumItems);
+                    .then(sumItems)
+                    .then(result => result.getOrElse(0));
     }
 };
